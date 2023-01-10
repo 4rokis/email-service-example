ab484fd0179abb3999ff917853ad861d092e3dad
Bogdan Sikora <bogdan@arokis.me>
2023-01-10 12:16:28 +0100

    feat: Modal added

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+
+import { Modal } from './Modal'
+
+storiesOf('Modal', module).add('default', () => (
+  <Modal open onClose={action('onClose')}>
+    I am a modal!
+  </Modal>
+))
